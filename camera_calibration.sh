4ac154fa3b63c68784595346cb326e9999b7be89
Kyle McConnaughay <kyle@kyle-Latitude-E6420.(none)>
2014-12-01 12:27:33 -0500

    Automate camera_calibration

---
+++ camera_calibration.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+
+source catkin_utilities.sh
+
+
+install_camera_calibration() {
+    rosdep install camera_calibration
+    rosmake camera_calibration
+}
+
+
+check_camera_topics() {
+    topics=$(rostopic list)
+    if [[ $topics != *"/camera/camera_info"* ]]; then
+        echo "Error, line" ${LINENO} ": /camera/camera_info topic is not being published. Make sure to execute run_gscam.sh before running this file."
+        exit 1
+    fi
+
+    if [[ $topics != *"/camera/image_raw"* ]]; then
+        echo "Error, line" ${LINENO} ": /camera/image_raw topic is not being published. Make sure to execute run_gscam.sh before running this file."
+        exit 1
+    fi
+}
+
+
+run_camera_calibration() {
+    # See http://wiki.ros.org/camera_calibration/Tutorials/MonocularCalibration for instructions
+    rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.108 image:=/camera/image_raw camera:=/
+}
+
+
+rename_calibration_file() {
+    mv /home/$USER/.ros/camera_info/camera.yaml /home/$USER/.ros/camera_info/camera_calibration.yaml
+}
+
+
+install_camera_calibration
+check_camera_topics
+run_camera_calibration
+rename_calibration_file
